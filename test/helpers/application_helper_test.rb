8ea5f91bf714d873da8553ab36a61afa61b00206
taka-guchi <takahiro89baka@gmail.com>
2019-06-23 08:52:08 +0900

    Add unit test for helper method

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title helper' do
+    assert_equal full_title, 'Sample App'
+    assert_equal full_title('Help'), 'Help | Sample App'
+    assert_equal full_title('About'), 'About | Sample App'
+  end
+end
