6ee76f75bd8374c9de1cbb1372a169db44ecf58f
taka-guchi <takahiro89baka@gmail.com>
2019-06-24 00:20:15 +0900

    Add a integration test for saving valid user

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -14,4 +15,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger'
     assert_select 'form[action="/signup"]'
   end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert.alert-success', count: 1
+  end
+
 end
