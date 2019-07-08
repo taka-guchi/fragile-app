7779cf76c4827888dd58578effeedfb6408999b6
taka-guchi <takahiro89baka@gmail.com>
2019-07-08 21:58:40 +0900

    Add a friendly forwarding processing

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,10 +17,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', 'The form contains4 errors.'
   end
 
-  test 'successful edit' do
-    log_in_as(@user)
+  test 'successful edit friendly forwarding' do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert session[:forwarding_url].nil?
     name = 'Foo Bar'
     email = 'foo@bar.com'
     patch user_path(@user), params: { user: { name: name,
