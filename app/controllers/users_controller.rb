071a3a755e558754b100ed4853d1ccc42d6ea414
taka-guchi <takahiro89baka@gmail.com>
2019-07-08 23:28:22 +0900

    Apply pagy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  include Pagy::Backend
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def index
-    @users = User.all
+    @pagy, @users = pagy(User.all)
   end
 
   def show
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,6 @@
 module UsersHelper
+  include Pagy::Frontend
+
   # 引数で与えられたユーザーのGravatar画像を返す
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
