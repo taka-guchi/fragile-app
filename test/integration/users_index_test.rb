0027db076c209708b670d264c4598c03b4183785
taka-guchi <takahiro89baka@gmail.com>
2019-07-09 23:59:26 +0900

    Add some test for a destroy action

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,13 +4,23 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   include Pagy::Backend
 
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'nav.pagy-bootstrap-nav.pagination'
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as(@non_admin)
+    get users_path
+    assert 'a', text: 'delete', count: 0
   end
 end
