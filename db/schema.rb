b4f323920dcd671f676c3f30278803dd411bd654
taka-guchi <takahiro89baka@gmail.com>
2019-07-13 16:09:22 +0900

    Reset db

--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,6 @@ ActiveRecord::Schema.define(version: 2019_07_13_060737) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
