63bc1585b5d15c740fb2bc7ca548d0de29d6bb25
taka-guchi <takahiro89baka@gmail.com>
2019-07-14 21:03:53 +0900

    Add an action of destroying user's feeds

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,9 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
-    @misropost = current_user.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'Micropost created!'
       redirect_to root_url
@@ -12,11 +13,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
