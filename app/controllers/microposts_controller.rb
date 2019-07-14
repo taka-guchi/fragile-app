036553a8e2fff9f77c5bf64b4bf36e74889dcaed
taka-guchi <takahiro89baka@gmail.com>
2019-07-14 19:57:58 +0900

    Add a form for postting micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,21 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @misropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_page/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
