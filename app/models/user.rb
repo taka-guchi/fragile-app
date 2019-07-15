3d7af54059b1be1ebc9c5174e901743118b8e88d
taka-guchi <takahiro89baka@gmail.com>
2019-07-15 12:27:40 +0900

    Add a method of following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
     foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -32,6 +33,21 @@ class User < ApplicationRecord
   # フィード情報を取得する
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーのフォローを解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしていたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   # クラスメソッド
