5a7d4dc580cb6653cec88661780a1240fad482c0
taka-guchi <takahiro89baka@gmail.com>
2019-07-21 14:44:04 +0900

    Unfilter a password

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+
+# あえてfilter対象を何も指定しない
+Rails.application.config.filter_parameters += []
